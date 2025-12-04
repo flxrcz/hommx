8cb7204626a258c6174cb5a6d9b3d5dc980031e8
Felix Racz <racz@posteo.de>
2025-12-04 12:41:48 +0100

    chore: update examples

--- examples/hmm_vs_semi.py
+++ examples/hmm_vs_semi.py
@@ -1,8 +1,13 @@
 # %%
+import sys
+from pathlib import Path
+
+import adios4dolfinx
 import matplotlib.pyplot as plt
 import numpy as np
+import pyvista as pv
 import ufl
-from dolfinx import fem, mesh
+from dolfinx import fem, mesh, plot
 from dolfinx.fem.petsc import LinearProblem
 from mpi4py import MPI
 from petsc4py import PETSc
@@ -10,17 +15,27 @@ from petsc4py import PETSc
 from hommx.helpers import PoissonFEM
 from hommx.hmm import PoissonHMM, PoissonSemiHMM
 
-eps = 1 / 2**5
+eps = 0.9 * 1 / 2**3
 BOX = 5
-
+mesh_file = Path("reference_mesh.bp")
 
 # def A(x, y):
 #     return 0.33 + 0.15 * (ufl.sin(2 * ufl.pi * x[0]) + ufl.sin(2 * ufl.pi * y[0]))
 
 
+# def A(x, y):
+# return 1
+# return ufl.conditional(ufl.sin(2 * ufl.pi * y[0]) > 0, 10, 1)
+
+
 def A(x, y):
-    # return 1
-    return ufl.conditional(ufl.sin(2 * ufl.pi * y[0]) > 0, 10, 1)
+    # return ufl.as_matrix(
+    #     [
+    #         [ufl.conditional(ufl.sin(16 * ufl.pi * y[0]) > 0, 10, 1), 0],
+    #         [0, ufl.conditional(ufl.sin(16 * ufl.pi * y[0]) > 0, 10, 1)],
+    #     ]
+    # )
+    return ufl.conditional(ufl.cos(2 * ufl.pi * y[0]) < 0, 10, 1)
 
 
 def f(x):
@@ -41,19 +56,6 @@ def theta(x):
     return ufl.as_vector([x_0, x_1])
 
 
-# def Dtheta(x):
-#     arg_0 = ufl.pi/2 * x[0]
-#     arg_1 = ufl.pi/2 * x[1]
-#     f = theta_factor * ufl.cos(arg_0) * ufl.cos(arg_1)
-#     df_dx0 = -theta_factor * (ufl.pi/2) * ufl.sin(arg_0) * ufl.cos(arg_1)
-#     df_dx1 = -theta_factor * (ufl.pi/2) * ufl.cos(arg_0) * ufl.sin(arg_1)
-
-#     return ufl.as_matrix([
-#         [1 - x[1] * df_dx0, -f - x[1] * df_dx1],
-#         [f + x[0] * df_dx0, 1 + x[0] * df_dx1]
-#     ])
-
-
 def Dtheta(x):
     arg_0 = ufl.pi / 2 * x[0]
     arg_1 = ufl.pi / 2 * x[1]
@@ -67,17 +69,19 @@ def Dtheta(x):
 
 
 # def theta(x):
-#     x_0 = x[1]
-#     x_1 = x[0]
+#     x_0 = x[0]
+#     x_1 = x[1]
 #     return ufl.as_vector([x_0, x_1])
 
+
 # def Dtheta(x):
-#     return ufl.as_matrix([[1,0],[0,1]])
+#     return ufl.as_matrix([[1, 0], [0, 1]])
 
 
 # %%
 # calculate reference solution
 print("Calculating reference solution")
+sys.stdout.flush()
 
 
 def A_fem(x):
@@ -85,9 +89,11 @@ def A_fem(x):
     # return A(x, x/eps)
 
 
-N_ref = 2 ** (11)
+N_ref = 2 ** (8) + 1
 # msh = mesh.create_unit_square(MPI.COMM_WORLD, nx, nx)
 msh_ref = mesh.create_rectangle(MPI.COMM_WORLD, np.array([[0, 0], [BOX, BOX]]), [N_ref, N_ref])
+print("Writing reference mesh")
+adios4dolfinx.write_mesh(mesh_file, msh_ref)
 V_ref = fem.functionspace(msh_ref, ("Lagrange", 1))
 u = ufl.TrialFunction(V_ref)
 v = ufl.TestFunction(V_ref)
@@ -109,15 +115,21 @@ facets = mesh.locate_entities_boundary(
 dofs = fem.locate_dofs_topological(V_ref, entity_dim=1, entities=facets)
 bc = fem.dirichletbc(value=PETSc.ScalarType(0), dofs=dofs, V=V_ref)
 bcs = [bc]
-lp = LinearProblem(lhs, rhs, bcs)
+
+lp = LinearProblem(lhs, rhs, bcs, petsc_options={"ksp_type": "cg", "pc_type": "ilu"})
+print("Created reference problem")
+sys.stdout.flush()
 u_fem_ref = lp.solve()
+print("solved reference problem")
+sys.stdout.flush()
+adios4dolfinx.write_function(mesh_file, u_fem_ref, name=f"reference_{eps}_{N_ref}")
 # def Dtheta(x):
 #     return ufl.as_matrix([[1,0],[0,1]])
 
 
-# %%
+# %% homogenized solutions
 print("Calculating homogenized solutions")
-Ns = np.array([3, 4, 5, 6, 7, 8, 10, 15, 20, 30, 40, 50], dtype=int)
+Ns = np.array([2, 4, 8, 10, 15, 20], dtype=int)
 # Ns = np.logspace(1, 1.7, 10, dtype=int)
 Vs = []
 phmm_sols = []
@@ -128,7 +140,7 @@ for N in Ns:
     ny = N
     # msh = mesh.create_unit_square(MPI.COMM_WORLD, nx, nx)
     msh = mesh.create_rectangle(MPI.COMM_WORLD, np.array([[0, 0], [BOX, BOX]]), [nx, nx])
-    msh2 = mesh.create_unit_square(MPI.COMM_SELF, nx, nx)
+    msh2 = mesh.create_unit_square(MPI.COMM_SELF, nx, 70)
     phmm = PoissonHMM(msh, A, f, msh2, eps, petsc_options_cell_problem={"ksp_atol": 1e-9})
     phmmsemi = PoissonSemiHMM(
         msh, A, f, msh2, eps, Dtheta, petsc_options_cell_problem={"ksp_atol": 1e-9}
@@ -143,8 +155,7 @@ for N in Ns:
     pfem_sols.append(u_fem)
 
 
-# %%
-# interpolate hmm onto fine grid
+# %% interpolate hmm onto fine grid
 print("Interpolating onto fine grid")
 
 
@@ -168,9 +179,12 @@ for V, phmm_sol, phmmsemi_sol, pfem_sol in zip(Vs, phmm_sols, phmmsemi_sols, pfe
     phmm_sols_interpolated.append(interpolate_nonmatching(V_ref, V, phmm_sol))
     phmmsemi_sols_interpolated.append(interpolate_nonmatching(V_ref, V, phmmsemi_sol))
     pfem_sols_interpolated.append(interpolate_nonmatching(V_ref, V, pfem_sol))
+    adios4dolfinx.write_function(mesh_file, phmm_sols_interpolated[-1])
+    adios4dolfinx.write_function(mesh_file, phmmsemi_sols_interpolated[-1])
+    adios4dolfinx.write_function(mesh_file, pfem_sols_interpolated[-1])
 
-# %%
-# calculate L2 error
+
+# %% calculate L2 error
 COMM = MPI.COMM_WORLD
 
 
@@ -246,17 +260,35 @@ if COMM.rank == 0:
     plt.loglog(1 / Ns, L2_errors_hmm_vs_semi)
     plt.show()
 # %%
-# from dolfinx import plot
-# import pyvista as pv
 
-# cells, types, x = plot.vtk_mesh(V_ref)
-# grid = pv.UnstructuredGrid(cells, types, x)
-# grid.point_data["u"] = u_fem_ref.x.array
-# grid.set_active_scalars("u")
-# plotter = pv.Plotter(notebook=True)
-# plotter.add_mesh(grid, show_edges=True)
-# warped = grid.warp_by_scalar()
-# plotter.add_mesh(warped)
+cells, types, x = plot.vtk_mesh(V_ref)
+grid = pv.UnstructuredGrid(cells, types, x)
+grid.point_data["u"] = u_fem_ref.x.array
+grid.set_active_scalars("u")
+plotter = pv.Plotter(notebook=True)
+plotter.add_mesh(grid, show_edges=True)
+warped = grid.warp_by_scalar()
+plotter.add_mesh(warped)
 
-# plotter.show()
+plotter.show()
+# %%
+mesh_ref_read = adios4dolfinx.read_mesh(mesh_file, comm=MPI.COMM_WORLD)
+V_ref_read = fem.functionspace(mesh_ref_read, ("Lagrange", 1))
+u_fem_ref_read = fem.Function(V_ref_read)
+mesh_ref_function = adios4dolfinx.read_function(
+    mesh_file, u_fem_ref_read, name=f"reference_{eps}_{N_ref}"
+)
+# %%
+
+
+cells, types, x = plot.vtk_mesh(V_ref_read)
+grid = pv.UnstructuredGrid(cells, types, x)
+grid.point_data["u"] = u_fem_ref_read.x.array
+grid.set_active_scalars("u")
+plotter = pv.Plotter(notebook=True)
+plotter.add_mesh(grid, show_edges=True)
+warped = grid.warp_by_scalar()
+plotter.add_mesh(warped)
+
+plotter.show()
 # %%
